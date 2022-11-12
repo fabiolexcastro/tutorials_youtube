0873c1a098de94dbedc5e6fe85050b3d530d85bb
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2022-11-12 11:52:07 -0500

    Leaflet map

---
+++ populationMap/02 leaflet map.R
@@ -0,0 +1,99 @@
+
+# Load libraries ----------------------------------------------------------
+require(pacman)
+p_load(raster, terra, sf, tidyverse, leaflet, classInt, plotly, rnaturalearthdata, RColorBrewer, rmapshaper, geodata, rnaturalearth, gtools, glue, htmltools, mapview, leaflet.extras, fs)
+
+g <- gc(reset = T); rm(list = ls()); options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+shpf <- st_read('./gpkg/population.gpkg')
+shpf
+
+# To make the intervals ---------------------------------------------------
+brks <- classIntervals(var = shpf$total_2017, n = 6, style = 'quantile')
+brks <- brks$brks
+brks <- round(brks, digits = -2)
+brks[7] <- brks[7] + 600
+
+clss <- findInterval(x = pull(shpf, total_2017), vec = brks, all.inside = TRUE)
+shpf <- mutate(shpf, class = clss)
+lbls <- tibble(class = 1:6, min = brks[1:6], max = brks[2:7], interval = paste0(min, '-', max))
+
+# Join intervals with the shapefile ---------------------------------------
+shpf <- inner_join(shpf, lbls, by = 'class')
+shpf <- mutate(shpf, interval = factor(interval, levels = lbls$interval))
+shpf <- rmapshaper::ms_simplify(shpf)
+
+# Get the colors -----------------------------------------------------------
+# pal <- colorNumeric("viridis", NULL)
+paln <- brewer.pal(n = 6, name = 'YlOrRd')
+clrs <- tibble(clases = lbls$interval, paln)
+clrs <- mutate(clrs, clases = factor(clases, levels = lbls$interval))
+shpf <- mutate(shpf, clases = interval)
+shpf <- inner_join(shpf, clrs, by = 'clases')
+
+# Categorical (factor) -----------------------------------------------------
+
+leaflet() %>% 
+  addTiles() %>% 
+  addProviderTiles(providers$CartoDB.DarkMatter,group = "CartoDB") %>%
+  addPolygons(data = shpf,
+              stroke = TRUE, 
+              color = 'white', 
+              weight = 0.4, 
+              fillColor = shpf$paln, 
+              fillOpacity = 0.5,
+              smoothFactor = 0.5,
+              popup = paste0('Número de habiantes: ', shpf$total_2017, '<br>', 'Municipio:', ' ', str_to_title(shpf$MPIO_CNMBR), '<br>')) %>% 
+  addLegend('bottomright',
+            colors = clrs$paln,
+            labels = clrs$clases, 
+            title = 'Cantidad de habitantes') %>% 
+  addScaleBar(position = "bottomright",options = scaleBarOptions(maxWidth = 100,
+                                                                 metric = TRUE,
+                                                                 imperial = TRUE,
+                                                                 updateWhenIdle = TRUE)) %>%
+  addDrawToolbar(targetGroup = "Graficos", editOptions = editToolbarOptions(selectedPathOptions = selectedPathOptions()))%>%
+  addMeasure(position = "topleft",
+             primaryLengthUnit = "meters",
+             primaryAreaUnit = "sqmeters",
+             activeColor = "#3D535D",
+             completedColor = "#7D4479") %>% 
+  addSearchOSM() %>% 
+  addControlGPS() %>% 
+  addMiniMap(tiles = "CartoDB.DarkMatter", position = 'topleft') %>% 
+  addControl(html = "<p style = 'font-family:serif; font-weight:bold'><strong>Cantidad de habitantes a nivel municipal en Colombia (DANE - 2017)</strong></p>",
+             position = "topright")%>%
+  addControl(html = "<p style = 'font-family:serif; font-weight:bold'><strong>Autor: Fabio Castro</strong></p>",
+             position = "bottomright") %>% 
+  addResetMapButton() %>% 
+  mapshot(url = "./html/index.html", title = 'Población Colombia')
+
+# Continous ---------------------------------------------------------------
+
+pal <- colorNumeric(
+      brewer.pal(name = 'YlOrRd', n = 9),
+      domain = shpf$total_2017)
+
+pal <- colorNumeric('OrRd', domain = shpf$total_2017)
+pal <- colorNumeric(palette = 'YlGnBu', domain = shpf$total_2017)
+qpal <- colorQuantile("YlOrRd", shpf$total_2017, n = 7)
+
+leaflet() %>% 
+    addTiles() %>% 
+    addProviderTiles(providers$CartoDB.DarkMatter,group = "CartoDB") %>%
+    addPolygons(data = shpf,
+                stroke = TRUE, 
+                color = 'white', 
+                weight = 0.4, 
+                label = ~shpf$total_2017,
+                fillColor =  ~pal(shpf$total_2017), #colorNumeric(brewer.pal(name = 'YlOrRd', n = 9), shpf$total_2017) 
+                fillOpacity = 0.5,
+                smoothFactor = 0.5,
+                popup = paste0('Número de habiantes: ', shpf$total_2017, '<br>', 'Municipio:', ' ', str_to_title(shpf$MPIO_CNMBR), '<br>')) %>%
+    addLegend('bottomright',
+              values = shpf$total_2017,
+              labels = shpf$total_2017,
+              # labFormat = labelFormat(suffix = 'miles'),
+              pal = pal,
+              title = 'Cantidad de habitantes') 
